589633fbd703453e8fbcbf0e74387e2ec542b88e
Ram Sthapit <ramsthapit50@gmail.com>
2023-01-02 22:22:47 +0545

    urls & views

---
+++ base/urls/location_urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from base.views import location_views as views
+
+urlpatterns = [
+    path('', views.getLocations),
+    path('create', views.sendLocation),
+    path('<str:pk>/update/', views.updateLocation),
+    path('<str:pk>/delete/', views.deleteLocation),
+    path('<str:pk>', views.getLocation),
+]
