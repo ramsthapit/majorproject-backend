41a24e94fa73ea512d1798b3ad135518a556aaa2
Ram Sthapit <ramsthapit50@gmail.com>
2023-01-02 19:56:55 +0545

    custom user models

--- base/models.py
+++ base/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class User(AbstractBaseUser):
+  name = models.CharField(max_length=255)
+  email = models.CharField(max_length=255, unique=True)
+  password = models.CharField(max_length=255)
+  username = None
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+
+  def __str__(self):
+    return str(self.email)
 
 class Geolocation(models.Model):
   # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
